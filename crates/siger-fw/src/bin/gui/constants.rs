e59a46d91dcf9cca7b2e89c0201fd6391594f53b
reid <reid@scogg.in>
2025-10-13 15:31:27 -0500

    brighter buttons ig

--- crates/siger-fw/src/bin/gui/constants.rs
+++ crates/siger-fw/src/bin/gui/constants.rs
@@ -12,19 +12,18 @@ pub const RAW_Y_MIN: u16 = 0;
 pub const RAW_Y_MAX: u16 = 0x0120;
 pub const MIRROR_X: bool = true;
 
-pub const COLOR_BACKGROUND: Rgb565 = Rgb565::new(0x02, 0x02, 0x06);
-pub const COLOR_BUTTON: Rgb565 = Rgb565::new(6, 12, 6);
-pub const COLOR_BUTTON_BORDER: Rgb565 = Rgb565::new(2, 4, 2);
-pub const COLOR_BUTTON_ACTIVE: Rgb565 = Rgb565::new(28, 52, 28);
+pub const COLOR_BACKGROUND: Rgb565 = Rgb565::new(3, 6, 10);
+pub const COLOR_BUTTON: Rgb565 = Rgb565::new(12, 24, 18);
+pub const COLOR_BUTTON_BORDER: Rgb565 = Rgb565::new(6, 12, 10);
+pub const COLOR_BUTTON_ACTIVE: Rgb565 = Rgb565::new(18, 40, 28);
 pub const COLOR_TEXT: Rgb565 = Rgb565::WHITE;
-pub const COLOR_UNLOCK_BG: Rgb565 = Rgb565::new(1, 46, 22);
-pub const COLOR_CONFIRM_APPROVE: Rgb565 = Rgb565::new(4, 12, 4);
-pub const COLOR_CONFIRM_APPROVE_ACTIVE: Rgb565 = Rgb565::new(0, 45, 0);
-pub const COLOR_CONFIRM_REJECT: Rgb565 = Rgb565::new(18, 2, 2);
-pub const COLOR_CONFIRM_REJECT_ACTIVE: Rgb565 = Rgb565::new(31, 10, 10);
+pub const COLOR_UNLOCK_BG: Rgb565 = Rgb565::new(2, 28, 18);
+pub const COLOR_CONFIRM_APPROVE: Rgb565 = Rgb565::new(5, 50, 15);
+pub const COLOR_CONFIRM_APPROVE_ACTIVE: Rgb565 = Rgb565::new(4, 60, 18);
+pub const COLOR_CONFIRM_REJECT: Rgb565 = Rgb565::new(24, 8, 8);
+pub const COLOR_CONFIRM_REJECT_ACTIVE: Rgb565 = Rgb565::new(29, 8, 9);
 
 pub const SPINNER_FRAMES: &[char] = &['|', '/', '-', '\\'];
-
 pub const BUTTON_STABLE_DURATION: Duration = Duration::from_millis(30);
 pub const BUTTON_INACTIVE_GRACE: Duration = Duration::from_millis(80);
 pub const MIN_PRESS_DURATION: Duration = Duration::from_millis(40);
